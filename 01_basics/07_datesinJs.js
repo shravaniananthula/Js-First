9cb292b9b9edd923ba17cfddab65b01169901607
shravaniananthula <35376752+shravaniananthula@users.noreply.github.com>
2025-02-21 20:57:25 +0000

    date

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,4 @@
+let myDate = new Date() //this is object/instance of date
+console.log(myDate)
+//objects/instances have been created from a constructor function
+//constructor function serves as the blueprint, and each object created from it is an instance
