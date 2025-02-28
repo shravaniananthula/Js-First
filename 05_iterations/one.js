5372498f1aa3a86ccf7d937b436ba275497f5106
shravaniananthula <35376752+shravaniananthula@users.noreply.github.com>
2025-02-28 16:50:54 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,7 @@
 //for
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i <= 10; i++) { //control flow-initializes i = 0, then checks i condition and enters loop,
+    //performs function or necessary statements and then before exiting loop goes to increment
     const element = i;
     if(element==5){
         console.log("5 is best")
@@ -18,8 +19,25 @@ for (let i = 1; i < 10; i++) {
 }
 
 let myArray = ["flash","batman","superman"]
-
-for (let index = 0; index < myArray.length; index++) {
+console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) { //array length counts from 0
     const element = myArray[index];
     console.log(element)
+}
+
+//break and continue
+
+for (let index = 0; index <= 20; index++) { //copy down--alt+shift +down arrow 
+    if(index ==5){
+        console.log("detected 5")
+        break;//comes out for once and for all
+    }
+    console.log(index)    
+}
+for (let index = 0; index <= 20; index++) {
+    if(index ==5){
+        console.log("detected 5")
+        continue; //condition skips for 1 time but doesnt come out of the loop
+    }
+    console.log(index)    
 }
