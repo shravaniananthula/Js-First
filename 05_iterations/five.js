80ecba688039a3d923a5063d688d5c1b51684c6c
shravaniananthula <35376752+shravaniananthula@users.noreply.github.com>
2025-02-28 22:35:12 +0000

    yes

---
+++ 05_iterations/five.js
@@ -0,0 +1,5 @@
+const coding = ["JS","ruby","java","python","cpp"]
+
+coding.forEach( function (item){ //callback fntcn does not have name
+
+})
--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,6 +5,23 @@ const myObj = {
     swift:"swift by apple"
 }
 //for objects we use for in loop
+//maps do not use for in loop
 for (const key in myObj) {
     console.log(`${key} is shortcut for ${myObj[key]}`)
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    console.log(programming[key])
+}
+
+const map = new Map()//map is not iteratable
+map.set('IN', "india")
+map.set('US', "united states")
+map.set('FR', "france")
+map.set('IN', "india") //doesnt print india again as it takes unique values only
+
+for (const key in map) {
+   console.log(key)
 }
